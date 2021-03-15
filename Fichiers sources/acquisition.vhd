3a11a9391ad274529bd9e8c0a4cea967813f0605
b-chollet <78785949+b-chollet@users.noreply.github.com>
2021-03-15 17:19:36 +0100

    Nouvelle version correcte : acquisition

--- Fichiers sources/acquisition.vhd
+++ Fichiers sources/acquisition.vhd
@@ -31,6 +31,7 @@ entity acquisition is
   e_bp                  : in  std_logic_vector(4 downto 0); -- Bit croisant : droite, gauche, bas, haut  centre
   s_RW_mem              : out std_logic;                    -- Lecture/ecriture de la mémoire
   s_activation_memoire  : out std_logic;                    -- Activation de la R/W de la mémoire
+  s_fin_acquisition     : out std_logic;
   s_data                : out std_logic_vector(7 downto 0); -- Donnée à envoyé à la mémoire
   s_adresse             : out std_logic_vector(7 downto 0)  -- Adresse à laquelle envoyé la donnée en mémoire
   );
@@ -42,17 +43,18 @@ architecture Behavioral of acquisition is
 TYPE state IS (init,wait_push_b,load_mem,write_mem, calculate_new_addr);
 SIGNAL next_state, current_state : state;
 
-signal save_button                     : std_logic_vector(4 downto 0);
-signal save_addr, new_addr             : unsigned (7 downto 0);
-
-signal cpt                             : std_logic;
+signal save_button           : std_logic_vector(4 downto 0);
+signal save_addr, new_addr   : signed (7 downto 0);
+signal cpt, etat_calculate   : std_logic;
+signal activation_adresse   : std_logic;
 
 begin
 
   FSM : process (clk, rst) is
       begin
         if rst = '1' then
-            current_state <= init;
+            current_state     <= init;
+            
         elsif clk'event and clk = '1' then
                 current_state <= next_state;
         end if;
@@ -66,73 +68,88 @@ begin
           next_state <= wait_push_b;
 
         when wait_push_b =>
-          if e_bp /= "00000" then
+          if e_bp /= "00000" and e_bp'event then
             next_state <= load_mem;
           else
             next_state <= wait_push_b;
           end if;
 
         when load_mem =>
-          next_state <= write_mem;
+          if e_data'event then 
+            next_state <= write_mem;
+          else 
+            next_state <= load_mem;
+          end if;
 
         when write_mem =>
           if cpt = '1' then
             next_state <= wait_push_b;
-          else
-            next_state <= write_mem;
-          end if;
-
-          if e_data'event then
+          elsif cpt = '0' then
             next_state <= calculate_new_addr;
           else
             next_state <= write_mem;
           end if;
 
         when calculate_new_addr =>
-            next_state <= calculate_new_addr;
+            if activation_adresse ='1' then 
+                next_state <= load_mem;
+            else 
+                next_state <= calculate_new_addr;
+            end if;
 
         when others => null;
       end case;
   end process;
 
-  etat_fsm : process(current_state)
+  etat_fsm : process(current_state,cpt,etat_calculate,new_addr,save_addr,activation_adresse)
     begin
       case current_state is
         when init =>
         -- On init tous les paramètres
-          save_addr   <= "99";
-          new_addr    <= "99";
-          save_button <= (others => '0');
-          cpt         <= '0';
+          save_addr            <= x"63";
+          save_button          <= (others => '0');
+          s_RW_mem             <= '0';
+          s_activation_memoire <= '0';
+          cpt                  <= '0';
+          s_data               <= "00000000";
+          s_adresse            <= "00000000";
+          s_fin_acquisition    <= '0';
+          etat_calculate       <= '0';
 
         when wait_push_b =>
         -- On remet à zéros cpt, et on attend l'appuie sur un bouton
-          cpt         <= '0';
-
+          if cpt ='1' then
+            cpt               <= not(cpt);
+            s_fin_acquisition <= '1';
+          end if;
+          s_activation_memoire <= '0'; -- A voir avec Adri
+          s_RW_mem             <= '0';
 
         when load_mem =>
         -- On affecte l'adresse sauvegardée à la sortie pour pouvoir lire en mémoire, le bloc associé
         -- /!\ il faut attendre d'avoir reçu le "bloc" avant de passer à l'état suivant, voir ligne 78
           save_button          <= e_bp;
-          s_adresse            <= save_addr;
+          s_adresse            <= std_logic_vector(save_addr);
           s_RW_mem             <= '0'; -- Norlamement, lecture
           s_activation_memoire <= '1'; -- A voir avec Adri
+          s_fin_acquisition    <= '0';
+          s_fin_acquisition    <= '1';
 
         when write_mem =>
         -- On écrit en mémoire le nouveau bloc
-          -- Case blanche vierge    : X'0'
-          -- Case noire vierge      : X'1'
-          -- Pion marron            : X'2'
-          -- Dame marron            : X’3’
-          -- Pion blanc             : X’4’
-          -- Dame blanche           : X’5’
-          -- Sélection case blanche : X'6'
-          -- Sélection case marron  : X'7'
-          -- Sélection pion marron  : X'8'
-          -- Sélection dame marron  : X'9'
-          -- Sélection pion blanc   : X'A'
-          -- Sélection dame blanche : X'B'
-          case save_button is
+          -- Case blanche vierge    : X'00'
+          -- Case noire vierge      : X'01'
+          -- Pion marron            : X'02'
+          -- Dame marron            : X'03'
+          -- Pion blanc             : X'04'
+          -- Dame blanche           : X'05'
+          -- Sélection case blanche : X'06'
+          -- Sélection case noire   : X'07'
+          -- Sélection pion marron  : X'08'
+          -- Sélection dame marron  : X'09'
+          -- Sélection pion blanc   : X'0A'
+          -- Sélection dame blanche : X'0B'
+          case e_data is
             when X"00" =>
               s_data               <= X"06";
               s_RW_mem             <= '1'; -- Norlamement, écriture
@@ -174,97 +191,69 @@ begin
           end case ;
 
         when calculate_new_addr =>
-          case save_button is
-            when "00001"  =>
-                if (signed(new_addr) + 1) mod 10 = 0 then
-                    signed(new_addr) := signed(new_addr) - 9;
-                else
-                    signed(new_addr) := signed(new_addr) + 1;
-                end if;
-                s_adresse <= std_logic_vector(new_addr);
-            when "00010"  =>
-                if signed(new_addr)  mod 10 = 0 then
-                    signed(new_addr) := signed(new_addr) + 9;
-                else
-                    signed(new_addr) := signed(new_addr) - 1;
-                end if;
-                s_adresse <= std_logic_vector(new_addr);
-            when "00100"  =>
-                if (89 < signed(new_addr)) and (signed(new_addr) < 100) then
-                    signed(new_addr) := signed(new_addr) - 90;
-                else
-                    signed(new_addr) := signed(new_addr) + 10;
-                end if;
-                s_adresse <= std_logic_vector(new_addr);
-            when "01000"  =>
-                if (-1 < signed(new_addr)) and (signed(new_addr) < 10) then
-                    signed(new_addr) := signed(new_addr) + 90;
-                else
-                    signed(new_addr) := signed(new_addr) - 10;
-                end if;
-                s_adresse <= std_logic_vector(new_addr);
-            when others   =>
-              s_adresse <= std_logic_vector(new_addr);
-          end case;
-
-          s_activation_memoire <= '0';
-          save_addr            <= new_addr;
-          cpt                  <= '1';
+            etat_calculate       <= '1';
+            s_activation_memoire <= '0';
+            s_RW_mem             <= '0';
+            cpt                  <= '1';
+            if activation_adresse = '1' then
+                save_addr          <= new_addr;
+                s_adresse          <= std_logic_vector(new_addr);
+                etat_calculate     <= '0';
+            end if;
 
         when others => null;
       end case;
 
   end process etat_fsm;
 
+
+    process (rst, clk,etat_calculate)
+    begin
+        if rst ='1' then 
+           new_addr           <= x"63";
+           activation_adresse <= '0';
+
+        elsif clk'event and clk ='1' then
+            if etat_calculate ='1' then
+             case save_button is
+               when "00001"  =>
+                   if (new_addr + 1 )mod 10 = 0 then
+                       new_addr <= new_addr - 9;
+                   else
+                       new_addr <= new_addr + 1;
+                   end if;
+                   activation_adresse <= '1';
+               when "00010"  =>
+                   if new_addr  mod 10 = 0 then
+                       new_addr <= new_addr + 9;
+                   else
+                       new_addr <= new_addr - 1;
+                   end if;
+                   activation_adresse <= '1';
+               when "00100"  =>
+                   if (89 < new_addr) and (new_addr < 100) then
+                       new_addr <= new_addr - 90;
+                   else
+                       new_addr <= new_addr + 10;
+                   end if;
+                   activation_adresse <= '1';
+               when "01000"  =>
+                   if -1 < new_addr and new_addr < 10 then
+                       new_addr <= new_addr + 90;
+                   else
+                       new_addr <= new_addr - 10;
+                   end if;
+                   activation_adresse <= '1';                  
+               when others   =>
+                 new_addr <= new_addr;
+                 activation_adresse <= '1';
+             end case;
+             
+             else 
+                activation_adresse <= '0';
+
+           end if; 
+        end if;
+    end process;
+
 end Behavioral;
-
-
-
-
-
-
-process (rst, clk)
-
-variable addr : integer range 0 to 99;
-
-begin
-    if rst = '1' then
-      s_adresse <= x"00";
-      s_data    <= x"00";
-      addr := 99; -- A définir la case par défaut
-
-    elsif clk'event and clk ='1' then
-        case e_bp IS
-            when "00001"  =>
-                if (addr + 1) mod 10 = 0 then
-                    addr := addr - 9;
-                else
-                    addr := addr + 1;
-                end if;
-                s_adresse <= std_logic_vector(to_unsigned(addr,8));
-            when "00010"  =>
-                if addr  mod 10 = 0 then
-                    addr := addr + 9;
-                else
-                    addr := addr - 1;
-                end if;
-                s_adresse <= std_logic_vector(to_unsigned(addr,8));
-            when "00100"  =>
-                if (89 < addr) and (addr < 100) then
-                    addr := addr - 90;
-                else
-                    addr := addr + 10;
-                end if;
-                s_adresse <= std_logic_vector(to_unsigned(addr+10,8));
-            when "01000"  =>
-                if (-1 < addr) and (addr < 10) then
-                    addr := addr + 90;
-                else
-                    addr := addr - 10;
-                end if;
-                s_adresse <= std_logic_vector(to_unsigned(addr,8));
-            when others   =>
-              s_adresse <= std_logic_vector(to_unsigned(addr,8));
-        end case;
-    end if;
-end process;
